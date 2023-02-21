7dfff2d04773c3df682327f5902a76b7d3066dde
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-21 10:42:25 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿var nomes = new List<string> { "Claudio", "Ana", "Felipe" };
+nomes.Add("Paulo");
+
+
+foreach(var nome in nomes)
+{
+    Console.WriteLine($"Olá {nome.ToUpper()}!");
+}
+
+Console.WriteLine($"Este nome  {nomes[1]} está na posição 1");
+
+var index = nomes.IndexOf("Tadeu");
+if(index == -1)
+{
+    Console.WriteLine($"Objeto não encontrado, IndexOf retornará {index}");
+}
+else
+{
+    Console.WriteLine($"O nome {nomes[index]} está na posição {index}");
+}
